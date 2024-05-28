b58da49f1a2f4b886376c34434899544236ebf46
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-05-28 22:07:32 +0000

    db connection started

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express')
 require('dotenv').config()
 const cors = require('cors')
+const mongoose = require('mongoose')
 const app = express()
 const PORT = process.env.PORT
+const db = mongoose.connection
+const db_name = process.env.DATABASE_NAME
 
 //middleware
 app.use(express.json())
 app.use(cors())
 
-//database s
+//database 
+mongoose.connect(db_name)
+db.on('error', () => console.log('Error when Connecting to database'))
+db.once('open', () => console.log('Successful in Connecting to database')) 
 
 
 app.listen(PORT, () => console.log(`Server is hosted on http://localhost:${PORT} `))
