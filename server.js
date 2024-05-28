e4e3e7e1df8804687a402d047470ce1c3c12459f
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-05-28 21:59:02 +0000

    adding middlewares

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express')
 require('dotenv').config()
+const cors = require('cors')
 const app = express()
 const PORT = process.env.PORT
+
+//middleware
+app.use(express.json())
+app.use(cors())
+
+//database s
 
 
 app.listen(PORT, () => console.log(`Server is hosted on http://localhost:${PORT} `))
