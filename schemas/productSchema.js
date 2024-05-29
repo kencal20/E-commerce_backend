1f2dea5c4a5a8ffafb875473c4dd121034e443f1
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-05-29 12:33:12 +0000

    product Schema update

--- schemas/productSchema.js
+++ schemas/productSchema.js
@@ -1,2 +1,30 @@
 const mongoose = require('mongoose')
 
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String
+    },
+    description: {
+        type: String
+    },
+    price: {
+        type: Number
+    },
+    category: {
+        type: String
+    },
+    stock: {
+        type: Number
+    },
+    imageUrl: {
+        type: String
+    }
+},
+    {
+        timestamps: true
+    }
+)
+
+const Product = mongoose.model('products', productSchema)
+
+module.exports = Product
