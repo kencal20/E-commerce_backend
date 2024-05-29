05b8c907ebdd737307ca5e0117cf02e2a9003f7c
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-05-29 23:37:15 +0000

    removal of password from schema because of firebase

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -10,10 +10,7 @@ const userSchema = new mongoose.Schema({
     required: true,
     unique: true,
   },
-  password: {
-    type: String,
-    required: true,
-  },
+  
   address: {
     type: String,
   },
