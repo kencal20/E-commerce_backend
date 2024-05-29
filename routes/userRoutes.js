0aa2d17f264d89e7f86ba86c34ce7a828f3d0b57
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-05-29 09:22:20 +0000

    post api created for user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,11 +2,26 @@ const express = require('express')
 const router = express.Router()
 const User = require('../schemas/userSchema')
 
-module.exports =  function () {
-    router.get('/',async (req, res) => {
+module.exports = function () {
+    router.get('/', async (req, res) => {
         const user = await User.find()
         res.json({ message: 'Hello all users', user })
     })
+
+    router.post('/create', async (req, res) => {
+        const { name, email, password, address, phone } = req.body
+        try {
+            const user = new User({
+                name, email, password, address, phone, role
+            })
+            const newUser = await user.save()
+            res.send({ message: 'New User Created', newUser })
+        } catch (error) {
+            const Error = error._message
+            res.send({ message: Error })
+        }
+    })
+
 
     return router
 }
