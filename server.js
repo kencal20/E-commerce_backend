27d3b2413a31c8ff4097216442625d817a5a3c92
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-05-29 22:40:05 +0000

    implementing firebase admin sdk and adding it to the userRoutes function call

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const db_name = process.env.DATABASE_NAME
 const userRouter = require('./routes/userRoutes')
 const productRoutes = require('./routes/productRoutes')
 const categoryRoutes = require('./routes/categoryRoutes')
+const admin = require('firebase-admin')
+
 //middleware
 app.use(express.json())
 app.use(cors())
@@ -18,8 +20,13 @@ mongoose.connect(db_name)
 db.on('error', () => console.log('Error when Connecting to database'))
 db.once('open', () => console.log('Successful in Connecting to database'))
 
+//firebase-admin setup
+const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT_KEY_PATH);
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+})
 
-app.use('/users/', userRouter())
+app.use('/users/', userRouter(admin))
 app.use('/products/', productRoutes())
 app.use('/categories/', categoryRoutes())
 
