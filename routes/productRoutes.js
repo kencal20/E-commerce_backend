af808bec503edc73f963ee494083bc0f2a17e9db
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-06-01 22:02:44 +0000

    creating a get api to fetch individual profducts

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,13 +2,25 @@ const express = require('express')
 const router = express.Router()
 const Product = require('../schemas/productSchema')
 const Category = require('../schemas/categorySchema')
+
 module.exports = function () {
     router.get('/', async (req, res) => {
         const products = await Product.find()
         res.json({ message: "This is the list of Products", products })
     })
 
-
+    router.get('/:id', async (req, res) => {
+        const { id } = req.params
+        try {
+            const product = await Product.findById(id)
+            if (!product) {
+                return res.json({ message: "The id Cannot be found" })
+            }
+            res.json({ messsage: "The result for the id inouted is ", product })
+        } catch (error) {
+            res.json({error:error.message})
+        }
+    })
     router.post('/create', async (req, res) => {
         const { productName, description, price, categoryName, stock, imageUrl } = req.body
         try {
