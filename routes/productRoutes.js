334ea9238c1c1cb29eb4489c77524481c812ae90
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-06-01 21:26:07 +0000

    update of get all api

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const Product = require('../schemas/productSchema')
-
+const Category = require('../schemas/categorySchema')
 module.exports = function () {
-    router.get('/', (req, res) => {
-        res.send({ message: "Welcome to products" })
+    router.get('/', async (req, res) => {
+        const products = await Product.find()
+        res.json({ message: "This is the list of Products", products })
     })
 
+
+ 
     return router
 }
