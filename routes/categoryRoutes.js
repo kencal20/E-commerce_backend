6bd2368289aca6ffd5bae613f14f0846e3eab20d
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-06-01 21:21:58 +0000

    update of get all api

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -2,10 +2,12 @@ const express = require('express')
 const router = express.Router()
 const Category = require('../schemas/categorySchema')
 
-module.exports = function(){
-    router.get('/',  (req, res) => {
-res.send({message:"Hello World"})
+module.exports = function () {
+    router.get('/', async (req, res) => {
+        const categories = await Category.find()
+        res.json({ message: "List of Categories are ", categories })
     })
+
 
     return router
 }
