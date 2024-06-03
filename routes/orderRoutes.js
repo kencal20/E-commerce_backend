f134ce290d38734a0925e4a10739f627a9b23a75
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-06-03 21:52:57 +0000

    add get all api

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -3,6 +3,11 @@ const router = express.Router()
 const Order = require('../schemas/orderSchema')
 
 module.exports = function(){
+    router.get('/', async (req, res) => {
+        const orders = await Order.find();
+        res.json({ message: 'List of Orders', orders });
+        
+    });
 
     return router;
 }
